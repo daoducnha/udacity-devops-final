dc173de9439cd2287b56be282c3c9fbcaed4174b
nhadd <aoducnha29493@gmail.com>
2022-08-03 23:48:02 +0700

    Update code

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def index():
-    return 'Hello World, my name is Nha!'
+    return 'Hello World, my name is Nha version 2!'
 
 app.run(host='0.0.0.0', port=80)
